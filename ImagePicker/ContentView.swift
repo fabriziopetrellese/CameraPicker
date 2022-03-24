ebf9d469f7ac8b1b7ec841d1ff31a23d21abb5ad
Fabrizio Petrellese <petrellesefabrizio@icloud.com>
2022-03-24 17:19:04 +0100

    change name

--- ImagePicker/ContentView.swift
+++ ImagePicker/ContentView.swift
@@ -6,12 +6,81 @@
 //
 
 import SwiftUI
+import PhotosUI
+
+//struct ContentView: View {
+//    @State var isPresented = false
+//
+//    var body: some View {
+//        VStack {
+//
+//            Button {
+//                isPresented.toggle()
+//            } label: {
+//                Text("Present Picker")
+//            }
+//        }
+//        .sheet(isPresented: $isPresented) {
+//            ImagePicker(sourceType: .photoLibrary)
+//        }
+//
+//    }
+//}
+
+//struct ContentView: View {
+//    @State private var isPresented = false
+//
+//    var body: some View {
+//        Button {
+//            isPresented.toggle()
+//        } label: {
+//            Text("Present Picker")
+//        }
+//        .sheet(isPresented: $isPresented) {
+//            let configuration = PHPickerConfiguration(photoLibrary: PHPhotoLibrary.shared())
+//            PhotoPicker(configuration: configuration, isPresented: $isPresented)
+//        }
+//    }
+//}
 
 struct ContentView: View {
+
+    @State private var isShowPhotoLibrary = false
+    @State private var image = UIImage()
+
     var body: some View {
-        Text("Hello, world!")
-            .font(.largeTitle)
-            .foregroundColor(.red)
+        VStack {
+
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFit()
+//                .frame(minWidth: 0, maxWidth: .infinity)
+//                .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 500)
+                .frame(width: 1 * UIScreen.main.bounds.width, height: 0.7 * UIScreen.main.bounds.height)
+                .edgesIgnoringSafeArea(.all)
+
+            Button(action: {
+                self.isShowPhotoLibrary = true
+            }) {
+                HStack {
+//                    Image(systemName: "photo")
+//                        .font(.system(size: 20))
+
+                    Text("Camera Picker")
+                        .font(.headline)
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
+//                .background(Color.blue)
+                .foregroundColor(.blue)
+//                .cornerRadius(20)
+                .padding(.horizontal)
+                .padding()
+            }
+        }
+        .fullScreenCover(isPresented: $isShowPhotoLibrary) {
+            ImagePicker(sourceType: .camera, selectedImage: self.$image)
+                .ignoresSafeArea()
+        }
     }
 }
 
---
+++ ImagePicker/ImagePicker.swift
@@ -0,0 +1,69 @@
+//
+//  ImagePicker.swift
+//  ImagePicker
+//
+//  Created by Fabrizio Petrellese on 23/03/22.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) private var presentationMode
+    var sourceType: UIImagePickerController.SourceType = .camera
+    @Binding var selectedImage: UIImage
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+    }
+}
+
+
+
+//struct ImagePicker: UIViewControllerRepresentable {
+//    @Environment(\.presentationMode) private var presentationMode
+//    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+//
+//    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+//
+//        let imagePicker = UIImagePickerController()
+//        imagePicker.allowsEditing = false
+//        imagePicker.sourceType = sourceType
+//
+//        return imagePicker
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+//}
